f395e071bff42dc05521305cde5e326943e4b3bb
vitorlolli <vitorlolli96@gmail.com>
2023-07-02 13:48:37 -0300

    função para cortar video

--- crop-video.js
+++ crop-video.js
@@ -1,28 +1,64 @@
 import fs from 'fs'
 import ffmpeg from 'fluent-ffmpeg'
 import ffmpegPath from '@ffmpeg-installer/ffmpeg'
+import { differenceInSeconds, setHours, setMinutes, setSeconds } from 'date-fns'
 
 ffmpeg.setFfmpegPath(ffmpegPath.path)
 
-const videoPath = '/home/vitor/Área de Trabalho/LAB/utilities/The.Office.S03E22.720p.BluRay.x264.DUAL-WWW.BLUDV.TV.mkv'
+const cropVideo = (videoPath, outputPath, startTime, endTime, options) => new Promise(async (resolve, reject) => {
+    const date = new Date()
 
-// opção para ser compativel com o whatsapp
-// .outputOptions([
-    // '-ac 2'
-// ])
+    const [startHour, startMinute, startSecond] = startTime.split(':')
+    let startTimeDate = setHours(date, startHour)
+    startTimeDate = setMinutes(startTimeDate, startMinute)
+    startTimeDate = setSeconds(startTimeDate, startSecond)
 
-ffmpeg({ source: videoPath })
-    .setStartTime('00:11:48.3')
-    .duration('12.5')
-    .output('video.mp4')
-    .outputOptions([
-        '-ac 2'
-    ])
-    .on('start', function (commandLine) {
-        console.log('Processing')
-    })
-    .on('end', function (err) {
-        if (!err) { console.log('conversion Done') }
-    })
-    .on('error', err => console.log('error: ', err))
-    .run()
+    const [endHour, endMinute, endSecond] = endTime.split(':')
+    let endTimeDate = setHours(date, endHour)
+    endTimeDate = setMinutes(endTimeDate, endMinute)
+    endTimeDate = setSeconds(endTimeDate, endSecond)
+
+    const diffSeconds = differenceInSeconds(
+        endTimeDate,
+        startTimeDate
+    )
+
+    const outputOptions = []
+    if (options.whatsAppPreview) {
+        outputOptions.push('-ac 2')
+    }
+
+    ffmpeg({ source: videoPath })
+        .setStartTime(startTime)
+        .duration(diffSeconds)
+        .output(outputPath)
+        .outputOptions(outputOptions)
+        .on('start', function (commandLine) {
+            console.log('Processing')
+        })
+        .on('end', function (err) {
+            if (!err) { 
+                console.log('conversion Done') 
+                resolve()
+            }
+            else {
+                reject()
+            }
+        })
+        .on('error', err => {
+            console.log('error: ', err)
+            reject()
+        })
+        .run()
+})
+
+const videoPath = ''
+
+await cropVideo(
+    videoPath, 
+    './video.mp4',
+    '00:26:16', 
+    '00:27:67', {
+        whatsAppPreview: true
+    }
+)
--- youtube-video-download.js
+++ youtube-video-download.js
@@ -6,7 +6,7 @@ import ffmpegPath from '@ffmpeg-installer/ffmpeg'
 
 ffmpeg.setFfmpegPath(ffmpegPath.path)
 
-const videoDownload = (id) => new Promise(async (resolve, reject) => {
+const videoDownload = id => new Promise(async (resolve, reject) => {
     let { videoDetails } = await ytdl.getInfo(id)
     const { title, video_url } = videoDetails
     const path = `./${slugify(title, '_')}.mp4`
@@ -20,30 +20,4 @@ const videoDownload = (id) => new Promise(async (resolve, reject) => {
         })
 })
 
-//const videoPath = await videoDownload('LU6yH8ppXX0')
-
-
-//corta video
-/*ffmpeg({ source: videoPath })
-    .setStartTime('00:01:12.5')
-    .output('video.mp4')
-    .on('start', function (commandLine) {
-        console.log('Processing')
-    })
-    .on('end', function (err) {
-        if (!err) { console.log('conversion Done') }
-    })
-    .on('error', err => console.log('error: ', err))
-    .run()*/
-
-// converte video para audio
-/*ffmpeg({ source: 'video.mp4' })
-    .output('audio.mp3')
-    .on('start', function (commandLine) {
-        console.log('Processing')
-    })
-    .on('end', function (err) {
-        if (!err) { console.log('conversion Done') }
-    })
-    .on('error', err => console.log('error: ', err))
-    .run()*/
+const videoPath = await videoDownload('xHCnrdf2NBI')
