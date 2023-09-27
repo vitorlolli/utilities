3cdcb5ef9d96efa4ec4a698ed0551072a0b77174
Vitor.Lolly <Vitor.Lolly@matriz.crosp.local>
2023-09-27 16:57:03 -0300

    refactor das funcões do youtube

---
+++ src/youtube.js
@@ -0,0 +1,39 @@
+import fs from 'fs'
+import path from 'path'
+import ytdl from 'ytdl-core'
+import { filenamifyPath } from 'filenamify'
+import ytpl from 'ytpl'
+
+const videoDownload = ({ id, dir = path.resolve(), audioOnly = false }) => new Promise(async (resolve, reject) => {
+    let { videoDetails } = await ytdl.getInfo(id)
+    const { title, video_url } = videoDetails
+    const ext = !audioOnly ? 'mp4' : 'mp3'
+    const file_name = path.basename(filenamifyPath(`${title}.${ext}`))
+    const content_path = path.resolve(dir, file_name)
+    ytdl(video_url, { quality: 'highest', filter: !audioOnly ? 'audioandvideo' : 'audioonly' })
+        .pipe(fs.createWriteStream(content_path))
+        .on("finish", async () => {
+            resolve(content_path)
+        })
+        .on("error", (error) => {
+            reject(error)
+        })
+})
+
+const getYoutubePlaylistVideosIds = async playlist_id => {
+    const ids = []
+    let result = await ytpl(playlist_id, { pages: 1 })
+    ids.push(...result.items.map(item => item.id))
+    let continuation = result.continuation
+    while (continuation) {
+        const next = await ytpl.continueReq(continuation)
+        ids.push(...next.items.map(item => item.id))
+        continuation = next.continuation
+    }
+    return ids
+}
+
+export default {
+    videoDownload,
+    getYoutubePlaylistVideosIds
+}
--- youtube-playlist-download.js
+++ youtube-playlist-download.js
@@ -1,87 +1,29 @@
-import ytpl from 'ytpl'
 import fs from 'fs'
-import path from 'path'
-import ytdl from 'ytdl-core'
-import slugify from 'slugify'
 import ora from 'ora'
 import process from 'process'
 import chalk from 'chalk'
 import prettyMilliseconds from 'pretty-ms'
 import prettyBytes from 'pretty-bytes'
-import ffmpeg from 'fluent-ffmpeg'
-import ffmpegPath from '@ffmpeg-installer/ffmpeg'
 
-ffmpeg.setFfmpegPath(ffmpegPath.path)
+import youtube from "./src/youtube.js"
 
-const convertToMp3 = (videoPath, mp3Path) => new Promise(async (resolve, reject) => {
-    ffmpeg({ source: videoPath })
-        .saveToFile(mp3Path)
-        .on('end', function (err) {
-            if (!err) {
-                resolve()
-            }
-            else {
-                reject()
-            }
-        })
-        .on('error', err => {
-            console.log('error: ', err)
-            reject()
-        })
-        .run()
-})
-
-const getYoutubePlaylistVideosIds = async playlist_id => {
-    const ids = []
-    let result = await ytpl(playlist_id, { pages: 1 })
-    ids.push(...result.items.map(item => item.id))
-    let continuation = result.continuation
-    while (continuation) {
-        const next = await ytpl.continueReq(continuation)
-        ids.push(...next.items.map(item => item.id))
-        continuation = next.continuation
-    }
-    return ids
-}
-
-const videoDownload = (dir, id) => new Promise(async (resolve, reject) => {
-    let { videoDetails } = await ytdl.getInfo(id)
-    const { title, video_url } = videoDetails
-    const content_path = path.resolve(dir, `${slugify(title, { replacement: '_', lower: true })}.mp4`)
-    ytdl(video_url, { quality: 'highest', filter: 'audioandvideo' })
-        .pipe(fs.createWriteStream(content_path))
-        .on("finish", async () => {
-            resolve(content_path)
-        })
-        .on("error", (error) => {
-            reject(error)
-        })
-})
 const spinnerPlaylist = ora('Getting videos Ids').start()
-const videos_ids = await getYoutubePlaylistVideosIds('PLHJJa8p-g9sMYUjL_4YB8c5uOKOicquDB')
+const videos_ids = await youtube.getYoutubePlaylistVideosIds('PL8_BclP632-JPWueg3THhGUmLyse9OJhh')
 spinnerPlaylist.text = `${videos_ids.length} videos found`
 spinnerPlaylist.succeed()
 
 console.log('')
 
-const onlyAudio = true
+const audioOnly = false
 
 for (let index = 0; index < videos_ids.length; index++) {
     const video_id = videos_ids[index]
     const label = `${String(index + 1).padStart(String(videos_ids.length).length, "0")}/${videos_ids.length}`
     const start_time = process.hrtime()
-    const spinner = ora(`${label} downloading`).start()
-    const video_path = await videoDownload('./musicas', video_id)
+    const spinner = ora(`${label} downloading`).start()    
+    const video_path = await youtube.videoDownload({ dir: './musicas', id: video_id, audioOnly })
     spinner.text = `${label} download success`
     let final_file = video_path
-    if (onlyAudio) {
-        const path_info = path.parse(video_path)
-        const mp3_path = path.resolve(path_info.dir, `${path_info.name}.mp3`)
-        spinner.text = `${label} converting`
-        await convertToMp3(video_path, mp3_path)
-        final_file = mp3_path
-        // fs.unlinkSync(video_path)
-    }
     const file_size = prettyBytes(fs.statSync(final_file).size)
     const end_time = process.hrtime(start_time)
     const pretty_time = prettyMilliseconds(end_time[0] * 1000 + end_time[1] / 1000000)
