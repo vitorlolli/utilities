59db371e6a3038f2f80c9f2e4b3474513a21320b
vitorlolli <vitorlolli96@gmail.com>
2024-07-10 20:18:40 -0300

    ajustes para processamento em massa

--- face-recognition.js
+++ face-recognition.js
@@ -1,14 +1,29 @@
 import * as canvas from 'canvas'
 import * as faceapi from 'face-api.js'
-
+import fs from 'fs'
+import path from 'path'
 
 await faceapi.nets.ssdMobilenetv1.loadFromDisk('./face-api-models')
 
 const { Canvas, Image, ImageData } = canvas
 faceapi.env.monkeyPatch({ Canvas, Image, ImageData })
 
-const imagePath = ''
-const img = await canvas.loadImage(imagePath)
+const face_path = './result/face'
+const no_face_path = './result/no-face'
 
-const detections = await faceapi.detectAllFaces(img)
-console.log(detections)
+const getImages = () => fs.readdirSync('./input').map(image => path.join('./', 'input', image))
+
+const images = getImages()
+
+for (let index = 0; index < images.length; index++) {
+    const image_path = images[index]
+    const info = path.parse(image_path)
+    const img = await canvas.loadImage(image_path)
+
+    const detections = await faceapi.detectAllFaces(img)
+    const destiny = detections.length > 0 ? face_path : no_face_path
+
+    fs.renameSync(`${image_path}`, `${destiny}/${info.base}`)
+
+    console.log(`${images.length} / ${index}`, image_path, destiny)
+}
